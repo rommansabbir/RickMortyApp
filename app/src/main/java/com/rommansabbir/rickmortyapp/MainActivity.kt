5c13582ad07ad2c6372e66187adb2a210abbf09b
rommansabbir <rommansabbir@gmail.com>
2023-08-30 21:18:56 +0600

    init commit

---
+++ app/src/main/java/com/rommansabbir/rickmortyapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.rommansabbir.rickmortyapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rommansabbir.rickmortyapp.ui.theme.RickMortyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RickMortyAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RickMortyAppTheme {
+        Greeting("Android")
+    }
+}
