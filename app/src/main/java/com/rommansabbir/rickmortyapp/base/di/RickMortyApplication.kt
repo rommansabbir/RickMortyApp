6b7e7eeee3e813405c23b45ae89956a3f07e64e4
rommansabbir <rommansabbir@gmail.com>
2023-08-30 22:37:50 +0600

    application : RickMortyApplication added and DI Implemented

---
+++ app/src/main/java/com/rommansabbir/rickmortyapp/base/di/RickMortyApplication.kt
@@ -0,0 +1,7 @@
+package com.rommansabbir.rickmortyapp.base.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RickMortyApplication : Application()
